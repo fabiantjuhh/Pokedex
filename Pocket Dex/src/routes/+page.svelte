c5483aa1e1ab3616bd3db2dc508aa396da442e09
Fabian <deadlypixels@outlook.com>
2024-09-02 10:47:04 +0200

    switched from lythical API to PokéAPI -> modal is broken needs fix

--- Pocket Dex/src/routes/+page.svelte
+++ Pocket Dex/src/routes/+page.svelte
@@ -1,87 +1,40 @@
 <script>
 	import { onMount } from 'svelte';
 	import PokeModal from './modal.svelte';
-
-	/**
-	 * @type {any[]}
-	 */
+	// @ts-ignore
 	let pokemonList = [];
 	let loading = false;
+	// @ts-ignore
+	let selectedPokemon = null;
+	let showModal = false;
 	let searchTerm = '';
 	let selectedType = 'all'; // Default selection is "all"
-	/**
-	 * @type {any}
-	 */
-	let selectedPokemon;
-	let showModal = false;
+	let filteredPokemon = [];
 
-	const baseUrl = 'https://lythical1.github.io/public/api/';
-	// Currently, not all data is being utilized, mostly "All_Pokemon_Data.json".
+	const baseUrl = 'https://pokeapi.co/api/v2/';
 
 	async function fetchPokemonList() {
 		loading = true;
 
 		try {
-			const [
-				pokemonResponse,
-				contestEffectResponse,
-				eggGroupResponse,
-				encounterMethodResponse,
-				growthRateResponse,
-				locationResponse,
-				movesResponse,
-				shapeResponse,
-				typesResponse,
-				natureResponse
-			] = await Promise.all([
-				fetch(`${baseUrl}all_pokemon_data.json`),
-				fetch(`${baseUrl}all_contest_effect_data.json`),
-				fetch(`${baseUrl}all_egg_group_data.json`),
-				fetch(`${baseUrl}all_encounter_method_data.json`),
-				fetch(`${baseUrl}all_growth_rate_data.json`),
-				fetch(`${baseUrl}all_location_data.json`),
-				fetch(`${baseUrl}all_moves_data.json`),
-				fetch(`${baseUrl}all_shape_data.json`),
-				fetch(`${baseUrl}all_types_data.json`),
-				fetch(`${baseUrl}natures.json`)
-			]);
+			const response = await fetch(`${baseUrl}pokemon?limit=1302`); // Fetching the first 151 Pokémon
+			const data = await response.json();
+			const pokemonArray = data.results;
 
-			const [
-				pokemonData,
-				contestEffectData,
-				eggGroupData,
-				encounterMethodData,
-				growthRateData,
-				locationData,
-				movesData,
-				shapeData,
-				typesData,
-				natureData
-			] = await Promise.all([
-				pokemonResponse.json(),
-				contestEffectResponse.json(),
-				eggGroupResponse.json(),
-				encounterMethodResponse.json(),
-				growthRateResponse.json(),
-				locationResponse.json(),
-				movesResponse.json(),
-				shapeResponse.json(),
-				typesResponse.json(),
-				natureResponse.json()
-			]);
-			const pokemonArray = Object.values(pokemonData);
-
+			// @ts-ignore
 			const promises = pokemonArray.map(async (pokemon) => {
-				const spriteResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${pokemon.id}`);
-				const spriteData = await spriteResponse.json();
+				const pokemonResponse = await fetch(pokemon.url);
+				const pokemonData = await pokemonResponse.json();
 
 				return {
-					name: pokemon.name,
-					types: pokemon.types,
-					number: pokemon.id,
-					moves: pokemon.moves,
-					baseExperience: pokemon.base_experience,
-					sprite: spriteData.sprites.front_default
+					name: pokemonData.name,
+					// @ts-ignore
+					types: pokemonData.types.map((typeInfo) => typeInfo.type.name),
+					number: pokemonData.id,
+					// @ts-ignore
+					moves: pokemonData.moves.map((moveInfo) => moveInfo.move.name),
+					baseExperience: pokemonData.base_experience,
+					sprite: pokemonData.sprites.front_default
 				};
 			});
 
@@ -102,6 +55,8 @@
 		selectedPokemon = pokemon;
 		showModal = true;
 	}
+
+	// @ts-ignore
 
 	$: filteredPokemon = [...pokemonList].filter((pokemon) => {
 		const lowerCaseName = pokemon.name.toLowerCase();
@@ -141,23 +96,25 @@
 </script>
 
 <main>
-    <nav class="flex-no-wrap top-0 flex flex-col md:flex-row w-full items-center justify-between z-10 py-2 shadow-md shadow-lg bg-rose-600 sticky p-4">
-        <a href="../" class="text-3xl font-bold">
-            <span class="text-white">The Poké Encyclopedia</span>
-        </a>
-        <div class="flex mt-4 md:mt-0">
-            <input
-                bind:value={searchTerm}
-                on:input={() => {}}
-                class="md:border-2 border-rose-400 bg-slate-200 h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none focus:border-rose-600"
-                type="search"
-                name="search"
-                placeholder="Search"
-            />
-            <select
-                bind:value={selectedType}
-                class="md:ml-2 md:border-2 border-rose-400 rounded-lg text-sm focus:outline-none focus:border-rose-600 mt-2 md:mt-0"
-            >
+	<nav
+		class="flex-no-wrap top-0 flex flex-col md:flex-row w-full items-center justify-between z-10 py-2 shadow-md shadow-lg bg-rose-600 sticky p-4"
+	>
+		<a href="../" class="text-3xl font-bold">
+			<span class="text-white">The Poké Encyclopedia</span>
+		</a>
+		<div class="flex mt-4 md:mt-0">
+			<input
+				bind:value={searchTerm}
+				on:input={() => {}}
+				class="md:border-2 border-rose-400 bg-slate-200 h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none focus:border-rose-600"
+				type="search"
+				name="search"
+				placeholder="Search"
+			/>
+			<select
+				bind:value={selectedType}
+				class="md:ml-2 md:border-2 border-rose-400 rounded-lg text-sm focus:outline-none focus:border-rose-600 mt-2 md:mt-0"
+			>
 				<option value="all">All Types</option>
 				<option value="normal">Normal</option>
 				<option value="fire">Fire</option>
@@ -187,7 +144,10 @@
 					{:else}
 						<ul class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
 							{#each filteredPokemon as { name, types, sprite, moves, baseExperience, number } (number)}
-								<button on:click={() => showPokemonDetails({ name, types, moves, baseExperience, sprite, number })}>
+								<button
+									on:click={() =>
+										showPokemonDetails({ name, types, moves, baseExperience, sprite, number })}
+								>
 									<div
 										class="flex-shrink-0 p-4 bg-gray-200 shadow-md shadow-gray-500/75 rounded-lg mx-auto"
 									>
